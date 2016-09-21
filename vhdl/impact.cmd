123d1a55f0e781c7f7d0ab8f7984e553ca967776
Dan Elkouby <streetwalrus@codewalr.us>
2016-09-21 23:00:23 +0300

    Makefile for VHDL

--- vhdl/impact.cmd
+++
@@ -1,5 +0,0 @@
-setMode -bs
-setCable -port xsvf -file "default.xsvf"
-addDevice -p 1 -file "gc_ipl.jed"
-Program -p 1 -e -v 
-quit
--- vhdl/program.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-source /opt/Xilinx/14.7/ISE_DS/settings64.sh
-
-impact -batch impact.cmd || exit 1
-time clujtag -p /dev/ttyACM1 -x default.xsvf
